a99b5c59b22bca65c349d9df125a682a82489105
Danh Han Antoree <hanminhdanh1325@gmail.com>
2021-11-16 20:39:37 +0700

    Init and install another library

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import DemoGrid from "./TailWindComponent/DemoGrid";
+import PaddingMarginDemo from "./TailWindComponent/PaddingMarginDemo";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <DemoGrid /> */}
+      <PaddingMarginDemo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TailWindComponent/PaddingMarginDemo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function PaddingMarginDemo() {
+  return (
+    <div className="container">
+      <div
+        className="bg-red-200 px-5 px-5"
+        style={{ width: 400, marginTop: 15 }}
+      >
+        lorem Ips lorem ipsum dolor sit lorem Ips lorem ipsum dolor sit lorem
+        Ips lorem ipsum dolor sit lorem Ips lorem ipsum dolor sit
+      </div>
+    </div>
+  );
+}
